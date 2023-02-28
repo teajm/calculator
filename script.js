d904323daa3132b9d349df6bdcb642e0793f6d0b
Taylor <taylormyrick@gmail.com>
2023-02-27 22:19:18 -0600

    adjusted formatting

--- script.js
+++ script.js
@@ -4,157 +4,156 @@ let storedNums2 = null;
 let resetFlag = false;
 let equalsFlag = false;
 let opsFlag = false;
-const numberButtons = document.querySelectorAll('[number]');
-const currentScreen = document.getElementById('screenCurrent');
-const lastScreen = document.getElementById('screenLast');
-const operatorButtons = document.querySelectorAll('[operator]');
-const equalsButton = document.querySelector('.equals');
-const clearButton = document.querySelector('.clear');
-const backspaceButton = document.querySelector('.backspace');
-const decimalButton = document.querySelector('.decimal');
+const numberButtons = document.querySelectorAll("[number]");
+const currentScreen = document.getElementById("screenCurrent");
+const lastScreen = document.getElementById("screenLast");
+const operatorButtons = document.querySelectorAll("[operator]");
+const equalsButton = document.querySelector(".equals");
+const clearButton = document.querySelector(".clear");
+const backspaceButton = document.querySelector(".backspace");
+const decimalButton = document.querySelector(".decimal");
 
 numberButtons.forEach((button) =>
-  button.addEventListener('click', () => appendNumber(button.textContent))
+	button.addEventListener("click", () => appendNumber(button.textContent))
 );
 
 operatorButtons.forEach((button) =>
-  button.addEventListener('click', () => storeOpsAndNums(button.textContent, currentScreen.textContent))
+	button.addEventListener("click", () =>
+		storeOpsAndNums(button.textContent, currentScreen.textContent)
+	)
 );
 
-window.addEventListener('keydown', (e) => interpretKeypress(e),true);
-equalsButton.addEventListener('click', () => evaluate());
-backspaceButton.addEventListener('click', () => deleteFromDisplay());
-clearButton.addEventListener('click', () => clearDisplay());
-decimalButton.addEventListener('click', () => addDecimal());
+window.addEventListener("keydown", (e) => interpretKeypress(e), true);
+equalsButton.addEventListener("click", () => evaluate());
+backspaceButton.addEventListener("click", () => deleteFromDisplay());
+clearButton.addEventListener("click", () => clearDisplay());
+decimalButton.addEventListener("click", () => addDecimal());
 
-function storeOpsAndNums(op, num){
-    screenLast.textContent = `${num} ${op} `
-    if(storedNums1 != null && storedNums2 == null){
-        storedNums2 = num;
-    }
-    if(opsFlag == true){
-        if(storedNums1 !== null && storedNums2 !==null && storedOps !=null){
-            evaluate(storedOps,storedNums1, currentScreen.textContent);
-        }
-    }
-    else{
-        storedNums1 = num;
-    }
-    
-    storedOps = op;
-    resetFlag = true;
-    equalsFlag = false;
-    opsFlag = true;
-}
+function storeOpsAndNums(op, num) {
+	screenLast.textContent = `${num} ${op} `;
+	if (storedNums1 != null && storedNums2 == null) {
+		storedNums2 = num;
+	}
+	if (opsFlag == true) {
+		if (storedNums1 !== null && storedNums2 !== null && storedOps != null) {
+			evaluate(storedOps, storedNums1, currentScreen.textContent);
+		}
+	} else {
+		storedNums1 = num;
+	}
 
-function appendNumber(num){
-    if(resetFlag === true){
-        resetDisplay();
-        resetFlag = false;
-    }
-    currentScreen.textContent += num;
+	storedOps = op;
+	resetFlag = true;
+	equalsFlag = false;
+	opsFlag = true;
 }
 
-function evaluate(){
-    if(storedNums1 != null && storedNums2 != null){
-        if(equalsFlag == true){
-            storedNums1 = currentScreen.textContent;
-        }
-        else{
-            storedNums2 = currentScreen.textContent;
-        }
-        if(storedNums1 !== null && storedNums2 !==null && storedOps !=null){
-            screenLast.textContent = `${storedNums1} ${storedOps} ${storedNums2} =`
-        }
-        currentScreen.textContent = roundNumber(operate(storedOps, storedNums1, storedNums2));
-    }
-    resetFlag = true;
-    equalsFlag = true;
-    opsFlag = false;
-    storedNums1 = currentScreen.textContent;
+function appendNumber(num) {
+	if (resetFlag === true) {
+		resetDisplay();
+		resetFlag = false;
+	}
+	currentScreen.textContent += num;
 }
 
-function resetDisplay(){
-    currentScreen.textContent = ' ';
+function evaluate() {
+	if (storedNums1 != null && storedNums2 != null) {
+		if (equalsFlag == true) {
+			storedNums1 = currentScreen.textContent;
+		} else {
+			storedNums2 = currentScreen.textContent;
+		}
+		if (storedNums1 !== null && storedNums2 !== null && storedOps != null) {
+			screenLast.textContent = `${storedNums1} ${storedOps} ${storedNums2} =`;
+		}
+		currentScreen.textContent = roundNumber(
+			operate(storedOps, storedNums1, storedNums2)
+		);
+	}
+	resetFlag = true;
+	equalsFlag = true;
+	opsFlag = false;
+	storedNums1 = currentScreen.textContent;
 }
 
-function clearDisplay(){
-    currentScreen.textContent = ' ';
-    screenLast.textContent = '';
-    storedOps = null;
+function resetDisplay() {
+	currentScreen.textContent = " ";
 }
 
-function deleteFromDisplay(){
-    currentScreen.textContent = currentScreen.textContent.toString().slice(0,-1);
+function clearDisplay() {
+	currentScreen.textContent = " ";
+	screenLast.textContent = "";
+	storedOps = null;
 }
 
-function addDecimal(){
-    if(resetFlag === true){
-        resetDisplay();
-        resetFlag = false;
-    }
-    if(currentScreen.textContent.includes('.')){
-        return;
-    }
-    currentScreen.textContent += '.';
+function deleteFromDisplay() {
+	currentScreen.textContent = currentScreen.textContent.toString().slice(0, -1);
 }
 
-function interpretKeypress(e){   
-    if(e.key >= 0  && e.key <= 9) appendNumber(e.key);
-    if(e.key == 'Backspace') deleteFromDisplay();
-    if(e.key == '.') addDecimal();
-    if(e.key == 'Enter') evaluate();
-    if(e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/'){
-        storeOpsAndNums(convertMathOp(e.key),currentScreen.textContent);
-    }
+function addDecimal() {
+	if (resetFlag === true) {
+		resetDisplay();
+		resetFlag = false;
+	}
+	if (currentScreen.textContent.includes(".")) {
+		return;
+	}
+	currentScreen.textContent += ".";
 }
 
-function convertMathOp(key){
-    if (key == '/'){
-        return '÷';
-    }
-    else if (key == '*'){
-        return '×';
-    }
-    else{
-        return key;
-    }
-}
-function roundNumber(num){
-    return Math.round(num * 1000) / 1000;
-}
-function add(num1, num2){
-    return Number(num1) + Number(num2);
-}
-function subtract(num1, num2){
-    return Number(num1) - Number(num2);
-}
-function multiply(num1, num2){
-    return Number(num1) * Number(num2);
-}
-function divide(num1, num2){
-    return Number(num1) / Number(num2);
-}
-function percent(a){
-    return divide(a,100);
-}
-function power(a,b){
-    return Math.pow(a,b);
-}
-  
-function operate(operator, num1, num2){
-    switch(operator){
-        case '+':
-            return add(num1,num2);
-        case '-':
-            return subtract(num1,num2);
-        case '×':
-            return multiply(num1,num2);
-        case '÷':
-            if(num2 === 0){
-                return null;
-            }
-            return divide(num1,num2);
-    }
+function interpretKeypress(e) {
+	if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+	if (e.key == "Backspace") deleteFromDisplay();
+	if (e.key == ".") addDecimal();
+	if (e.key == "Enter") evaluate();
+	if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") {
+		storeOpsAndNums(convertMathOp(e.key), currentScreen.textContent);
+	}
 }
 
+function convertMathOp(key) {
+	if (key == "/") {
+		return "÷";
+	} else if (key == "*") {
+		return "×";
+	} else {
+		return key;
+	}
+}
+function roundNumber(num) {
+	return Math.round(num * 1000) / 1000;
+}
+function add(num1, num2) {
+	return Number(num1) + Number(num2);
+}
+function subtract(num1, num2) {
+	return Number(num1) - Number(num2);
+}
+function multiply(num1, num2) {
+	return Number(num1) * Number(num2);
+}
+function divide(num1, num2) {
+	return Number(num1) / Number(num2);
+}
+function percent(a) {
+	return divide(a, 100);
+}
+function power(a, b) {
+	return Math.pow(a, b);
+}
+
+function operate(operator, num1, num2) {
+	switch (operator) {
+		case "+":
+			return add(num1, num2);
+		case "-":
+			return subtract(num1, num2);
+		case "×":
+			return multiply(num1, num2);
+		case "÷":
+			if (num2 === 0) {
+				return null;
+			}
+			return divide(num1, num2);
+	}
+}
